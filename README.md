07c1eb45bc2235ec65310b4a74d50bc62d600bb2
Amarjeet Anand <amarjeetanandsingh@gmail.com>
2020-08-23 19:30:18 +0530

    gen integration test setup(WIP) & pkgsite badge

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # TODO Fix document of whole project
 
 [![Build Status](https://travis-ci.org/amarjeetanandsingh/tgconst.svg?branch=master "Travis CI status")](https://travis-ci.org/github/amarjeetanandsingh/tgconst)
+[![PkgGoDev](https://pkg.go.dev/badge/github.com/amarjeetanandsingh/tgconst?tab=doc)](https://pkg.go.dev/github.com/amarjeetanandsingh/tgconst?tab=doc)
--- tests/gen_test.go
+++ tests/gen_test.go
@@ -1,1 +1,37 @@
 package tests
+
+import "testing"
+
+func TestGenCmd(t *testing.T) {
+	// test each flags
+	t.Run("all", genCmd_All)
+	t.Run("dir", genCmd_Dir)
+	t.Run("tags", genCmd_Tags)
+	t.Run("recursive", genCmd_Recursive)
+	t.Run("taggedFieldOnly", genCmd_TaggedOnly)
+	t.Run("MissingTagPolicy", genCmd_MiggingTagValPolicy)
+}
+
+func genCmd_All(t *testing.T) {
+
+}
+
+func genCmd_Dir(t *testing.T) {
+
+}
+
+func genCmd_Tags(t *testing.T) {
+
+}
+
+func genCmd_Recursive(t *testing.T) {
+
+}
+
+func genCmd_TaggedOnly(t *testing.T) {
+
+}
+
+func genCmd_MiggingTagValPolicy(t *testing.T) {
+
+}
